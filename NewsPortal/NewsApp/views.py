a172f94bbad4dca9ed71b3f82aa9b690ffbf4041
SergGT2023 <123579110+SergGT2023@users.noreply.github.com>
2023-04-26 12:22:23 +0300

    Update views.py

--- NewsPortal/NewsApp/views.py
+++ NewsPortal/NewsApp/views.py
@@ -56,3 +56,62 @@ def news_search(request):
         news_list = news_list.filter(date__gte=date)
 
     return render(request, 'news_search.html', {'news_list': news_list})
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import News, Article
+from .forms import NewsForm, ArticleForm
+
+def news_create(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news')
+    else:
+        form = NewsForm()
+    return render(request, 'news_form.html', {'form': form})
+
+def news_edit(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    if request.method == 'POST':
+        form = NewsForm(request.POST, instance=news)
+        if form.is_valid():
+            form.save()
+            return redirect('news_detail', pk=news.pk)
+    else:
+        form = NewsForm(instance=news)
+    return render(request, 'news_form.html', {'form': form})
+
+def news_delete(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    if request.method == 'POST':
+        news.delete()
+        return redirect('news')
+    return render(request, 'news_confirm_delete.html', {'news': news})
+
+def article_create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+    else:
+        form = ArticleForm()
+    return render(request, 'article_form.html', {'form': form})
+
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'article_form.html', {'form': form})
+
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles')
+    return render(request, 'article_confirm_delete.html', {'article': article})
