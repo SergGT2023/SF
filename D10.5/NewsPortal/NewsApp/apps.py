9069861913faefa384b97f52254d931f1b117600
SergGT2023 <123579110+SergGT2023@users.noreply.github.com>
2023-08-04 07:55:11 +0300

    Delete D10.5 directory

--- D10.5/NewsPortal/NewsApp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'NewsApp'
-
-    def ready(self):
-        import NewsApp.signals
